51a2fb49167492adc674dff1e280f6d4bed05a6d
nestify <nestify@12333153-ee01-4b9b-8cc9-3b69a7b36d39>
2008-04-14 17:29:05 +0000

    Udates to stroop (including analysis file) and a new mental rotation task.
    git-svn-id: svn://svn.code.sf.net/p/pebl/code-0/trunk@238 12333153-ee01-4b9b-8cc9-3b69a7b36d39

--- battery/stroop/stroop.R
+++ battery/stroop/stroop.R
@@ -2,6 +2,7 @@ datall <- read.table("stroop-0.txt")
 colnames(datall) <- c("sub","block","trial","type","cresp","cond","cond2","shade","word","resp","corr","respnum","start","rt")
 
 
+datall$testtype <- ceiling(datall$block/2)
 
 ##Just keep the correct trials and those after practice
 keep <- datall$corr==1 & datall$block > 0
@@ -10,14 +11,16 @@ dat <- datall[keep,]
 
 
 
-x <- exp(tapply(log(dat$rt),list(dat$cond2,dat$type,dat$cresp),mean))
+x <- exp(tapply(log(dat$rt),list(dat$cond2,dat$type,dat$testtype),mean))
 
-xlab <- c("Consistent","One\ndimension","Inconsistent")
+
+ord <- c(1,3,2)
+xlab <- c("Congruent","Incongruent","One\ndimension")
 par(mfrow=c(1,3))
-matplot(x[,,1],type="b",ylim=c(400,1600),main="Colored Word Responses",pch=c(16,18),cex=2,xaxt="n",ylab="Mean Response Time")
-axis(1,1:3,xlab)
-matplot(x[,,2],type="b",ylim=c(400,1600),main="Black Word Responses",pch=c(16,18),cex=2,xaxt="n",ylab="Mean Response Time")
-axis(1,1:3,xlab)
-matplot(x[,,3],type="b",ylim=c(400,1600),main="Colored Patch Responses",pch=c(16,18),cex=2,xaxt="n",ylab="Mean Response Time")
-axis(1,1:3,xlab)
+matplot(x[ord,,1],type="b",ylim=c(400,1600),main="Colored Word Responses",pch=c(16,18),cex=2,xaxt="n",ylab="Mean Response Time")
+axis(1,1:3,xlab[ord])
+matplot(x[ord,,2],type="b",ylim=c(400,1600),main="Black Word Responses",pch=c(16,18),cex=2,xaxt="n",ylab="Mean Response Time")
+axis(1,1:3,xlab[ord])
+matplot(x[ord,,3],type="b",ylim=c(400,1600),main="Colored Patch Responses",pch=c(16,18),cex=2,xaxt="n",ylab="Mean Response Time")
+axis(1,1:3,xlab[ord])
 legend(1,1400,c("ID Word","ID Color"),lty=c(1,2),col=c("black","red"),pch=c(16,18),cex=2,bty="n")
