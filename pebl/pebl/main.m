3c284b1f8541cb3212540a61b20d3042bcac6b17
nestify <nestify@12333153-ee01-4b9b-8cc9-3b69a7b36d39>
2012-09-07 02:08:32 +0000

    adding osx build files
    git-svn-id: svn://svn.code.sf.net/p/pebl/code-0/trunk@721 12333153-ee01-4b9b-8cc9-3b69a7b36d39

---
+++ pebl/pebl/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  pebl
+//
+//  Created by Elizabeth Veinott on 4/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
